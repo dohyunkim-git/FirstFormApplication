0febb36d9af67f39a1aba28d0d942d836d5410f6
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2020-05-19 22:02:16 +0900

    초기 커밋.

---
+++ FirstFormApplication/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            mybutton.Enabled = true;
+            mybutton.Text = "코드에서 변경!";
+            mybutton.Width = 200;
+            for (int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                button.Location = new Point(213, 13 + (23 + 3) * i);
+                Controls.Add(button);
+                button.Text = "동적 생성" + i + "번째";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
